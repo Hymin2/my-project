e41ccafc5422bd28b2e5002276d01b26fdd08ab2
khm8246 <khm8246@gmail.com>
2023-11-28 04:44:22 +0900

    feat: add SecurityUser that implements UserDetails

---
+++ src/main/java/com/myproject/global/security/SecurityUser.java
@@ -0,0 +1,51 @@
+package com.myproject.global.security;
+
+import com.myproject.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class SecurityUser implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getAuthorities().stream()
+                .map((authority) -> new SimpleGrantedAuthority(authority.getAuthorityName().toString()))
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
