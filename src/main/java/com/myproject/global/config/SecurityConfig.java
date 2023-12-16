ea2c842d17c5fe05f6f69042e5bb035766952586
khm8246 <khm8246@gmail.com>
2023-12-16 16:31:52 +0900

    feat: update SecurityConfig about authorization

--- src/main/java/com/myproject/global/config/SecurityConfig.java
+++ src/main/java/com/myproject/global/config/SecurityConfig.java
@@ -1,7 +1,13 @@
 package com.myproject.global.config;
 
+import com.myproject.global.security.AuthenticationProviderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -10,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -17,10 +24,16 @@ public class SecurityConfig {
                 .cors((cors) -> cors.disable())
                 .csrf((csrf) -> csrf.disable())
                 .authorizeHttpRequests((request) ->
-                        request.anyRequest()
+                        request.requestMatchers("/login")
+                                .permitAll()
+                                .requestMatchers("/register")
+                                .permitAll()
+                                .anyRequest()
                                 .authenticated())
-                .formLogin((login) ->
-                        login.successForwardUrl("home"))
+                .formLogin((login) -> login
+                        .successForwardUrl("/test")
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login"))
                 .build();
     }
 
